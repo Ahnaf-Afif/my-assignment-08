6e54cc6664796ed7dcdbfd78097256db8d21ff25
Ahnaf-Afif <williamhens123@gmail.com>
2026-05-15 21:53:02 +0600

    loading screen done

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const loading = () => {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex justify-center items-center h-screen bg-black">
       <span className="loading loading-bars loading-xl"></span>
     </div>
   );
